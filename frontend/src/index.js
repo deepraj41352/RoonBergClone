84f3b8220c69fc5cecf769835151b91bf09677e4
Gaytri Dodiya <dodiyadsvv@gmail.com>
2023-12-05 12:49:10 +0530

    createRoot import change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +9,8 @@ import reportWebVitals from './reportWebVitals';
 import StoreProvider from '../src/Store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'react-confirm-alert/src/react-confirm-alert.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
